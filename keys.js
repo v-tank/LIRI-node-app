c0b3d5510454834874c63d2233a4e3c0be281140
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-02-11 17:39:27 -0800

    initial commit

---
+++ keys.js
@@ -0,0 +1,10 @@
+console.log('Keys have been loaded!');
+
+var twitterKeys = {
+  consumer_key: '6TX8hQb4PnBytAfwxwGJn5QGc',
+  consumer_secret: 'zUdXGC6dZo0rcQAXRFp9WMVDKiPPocgJ2SFkbPrcCHwmJDZloy',
+  access_token_key: '3449525113-NBnS1Zd3Z1VtdOfEQspncCpTgPL26oKSC2yIHkd',
+  access_token_secret: 'qlS14fc0GXe9OkqAhwK48OzvbDNKE80uFWr49E7swRzYz',
+}
+
+module.exports = twitterKeys;
---
+++ liri.js
@@ -0,0 +1,37 @@
+var keys = require("./keys.js");
+
+var command = process.argv[2];
+console.log(command);
+
+switch (command) {
+    case "my-tweets":
+        break;
+    case "spotify-this-song":
+        break;
+    case "movie-this":
+        break;
+    case "do-what-it-says":
+        break;
+    default:
+        console.log("You didn't enter the right command. Please try again.");
+}
+
+function showMyTweets() {
+
+}
+
+function spotifyThisSong() {
+   
+}
+
+function movieInfo(movieName) {
+
+}
+
+function doWhatItSays() {
+
+}
+
+function appendToFile() {
+
+}
