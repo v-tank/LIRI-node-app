198adf252b8c27dbf66f067803bd4ecf1c041f83
Vaibhav Tank <vaibhav@Macbook-Pro.local>
2018-02-11 21:08:14 -0800

    added omdb key

--- keys.js
+++ keys.js
@@ -1,12 +1,11 @@
-console.log('Keys have been loaded!');
-
 var keys = {
   consumer_key: '6TX8hQb4PnBytAfwxwGJn5QGc',
   consumer_secret: 'zUdXGC6dZo0rcQAXRFp9WMVDKiPPocgJ2SFkbPrcCHwmJDZloy',
   access_token_key: '3449525113-NBnS1Zd3Z1VtdOfEQspncCpTgPL26oKSC2yIHkd',
   access_token_secret: 'qlS14fc0GXe9OkqAhwK48OzvbDNKE80uFWr49E7swRzYz',
   spotify_id: 'f82a4a97b31c4127a8f0b0fc499be8ed',
-  spotify_secret: 'd1f88afe80744d8ab842614731935745'
+  spotify_secret: 'd1f88afe80744d8ab842614731935745',
+  omdb_key: '40e9cece'
 }
 
 module.exports = keys;
